678acf862f6cd05006d72563ebb9f01fa9b8500f
cherra <jorge@matica.mx>
2013-09-17 12:51:45 -0500

    Commit inicial

---
+++ src/callbacks.h
@@ -0,0 +1,10 @@
+#include <gtk/gtk.h>
+
+
+void
+on_Pedidos_show                        (GtkWidget       *widget,
+                                        gpointer         user_data);
+
+void
+on_Pedidos_destroy                     (GtkObject       *object,
+                                        gpointer         user_data);
